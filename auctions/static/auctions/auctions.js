9c9c999ef82ea4431d3a6ebef3e536eb45d343df
Nicole Mah <mnmah@alaska.edu>
2022-11-09 16:37:10 -0900

    js not working

--- auctions/static/auctions/auctions.js
+++ auctions/static/auctions/auctions.js
@@ -1,23 +1,20 @@
-//test
 document.addEventListener("DOMContentLoaded", ()=> {
     lightbox()
     document.querySelectorAll('.watchlist-icon').forEach(icon => {
         var id = icon.dataset.id;
         icon.src = watchlist_check(icon.dataset.watchlist);
-        icon.onclick = ()=> {
-            fetch(`/api/watchlistToggle/${id}`).then(response => response.json()).then(data => {
-                console.log(data);
-                update_icon(data);
-    
-            })
-        }
+
     })
-    //const image = document.getElementById('watchlist');
-    //const id = image.dataset.id;
-    //image.src = watchlist_check(image.dataset.watchlist);
 
 })
+function icon_change(id,watchlist){
+    icon.src = watchlist_check(watchlist);
+    fetch(`/api/watchlistToggle/${id}`).then(response => response.json()).then(data => {
+        console.log(data);
+        update_icon(data);
 
+    })
+}
 function lightbox(){
     const imageInstance = basicLightbox.create(document.querySelector('#image'))
     document.querySelector('img.image').onclick = imageInstance.show
